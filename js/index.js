498409534cb11e0779eb4b9b599842a7001ed92b
Michal Kozlova <michal.ko@ITIM.LOCAL>
2020-02-24 11:35:50 +0200

    read more function started

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$('#readMore').click(function(){
+    // alert('im here');
+
+    $('#readMore').hide();
+
+    $('#feedbackList').append(`
+    <li class="timeline-inverted animated fadeInDown">
+        <div class="timeline-image">
+        <img class="rounded-circle img-fluid" src="img/feedback/gili.jpeg" alt="">
+        </div>
+        <div class="timeline-panel">
+        <div class="timeline-heading">
+            <h4>גילי
+            <a href="https://wa.me/972545755625" class="whatsapp rounded-circle" target="_blank">
+                <i class="fa fa-whatsapp my-whatsapp"></i>
+            </a>
+            </h4>
+            <h6 class="subheading">דצמבר, 2019</h6>
+        </div>
+        <div class="timeline-body">
+            <p class="text-muted text-justify">
+            ממליצה בחום ללמוד אצל אנה. מורה מקצועית מאוד ויחד עם זאת נעימה ואנושית. מצליחה לגרום להאמין ביכולת של כל אחד להצליח!
+            </p>
+        </div>
+        </div>
+    </li>
+`);
+
+});
